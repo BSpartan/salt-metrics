deae783e2269f7182f173e3a0f124bc368a3049d
christian calloway <christian@bloglovin.com>
2015-11-02 17:33:55 -0500

    feat( decorators ): added tests

---
+++ tests/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# christian@bloglovin
+# simple set of unit tests
+
+# imports #######################################
+
+import unittest
+import saltmetrics
+
+# tests #########################################
+
+class MainTest( unittest.TestCase ):
+  ''' Provides test cases for overall and expected functionality
+  '''
+  
+  def setUp( self ):
+    pass
+
+  def test_main( self ):
+    pass
+    
+  
+# main ##########################################
+
+if __name__ == '__main__':
+    unittest.main()
