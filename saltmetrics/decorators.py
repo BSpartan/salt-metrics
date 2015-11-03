c98123d62f2e8b18787170721d097046222e7135
christian calloway <christian@bloglovin.com>
2015-11-03 15:32:45 -0500

    feat( decorators ): decorator functionality working as expected under test cases

--- saltmetrics/decorators.py
+++ saltmetrics/decorators.py
@@ -6,6 +6,7 @@
 
 import json
 from   os.path import dirname
+from   .       import log
 
 # functions #####################################
 
@@ -18,49 +19,52 @@ class Decorate( object ):
     self.options   = options
 
   def __call__( self ):
+    # exec wrapped method, which should be BaseMetrics#save_metrics
+    self.method()
 
-    def prometheus( self ):
-      # exec wrapped method
-      self.method( self )
+    # finally call type specific method
+    getattr( self, self.decorator )()
 
-      # retrieve dump file and convert json to native dict
-      # instance
-      with open( self.options.metrics.saved_path, 'r' ) as file:
-        data      = json.loads( file.read() ) 
-        dump      = [ ]  
-        arguments = { }
 
-        # pass through dict object and retrieve all values 
-        # top level scalar values
-        for key, value in data:
-          if not isinstance( value, dict ):
-            arguments.append('{key}="{value}"'.format(
-              key   = key,
-              value = value 
+  def prometheus( self ):
+    # retrieve dump file and convert json to native dict
+    # instance
+    saved_path = self.options['metrics']['saved_path']
+
+    with open( saved_path, 'r' ) as file:
+      data      = json.loads( file.read() ) 
+      dump      = [ ]  
+      arguments = [ ]
+
+      # pass through dict object and retrieve all values 
+      # top level scalar values
+      for key, value in data.iteritems():
+        if not isinstance( value, dict ):
+          arguments.append('{key}="{value}"'.format(
+            key   = key,
+            value = value 
+          ))
+
+      arguments = '{' + (','.join( arguments )) + '}'
+
+      # pass through data again and "flatten" out all dict
+      # structures so we can easily dump into exposition 
+      # format
+      # NOTE: see ./examples/prometheus-exposition
+      for type, arbitrary in data.iteritems():
+        if isinstance( arbitrary, dict ):
+          for property, number in arbitrary.iteritems():
+            dump.append('salt_{type}_{property}{arguments} {number}'.format(
+              type      = type,
+              property  = property,
+              arguments = arguments,
+              number    = number
             ))
 
-        arguments = '{{args}}'.format(args = ','.join( arguments ))
-
-        # pass through data again and "flatten" out all dict
-        # structures so we can easily dump into exposition 
-        # format
-        # NOTE: see ./examples/prometheus-exposition
-        for type, arbitrary in data:
-          if isinstance( arbitrary, dict ):
-            for property, number in arbitrary:
-              dump.append('salt_{type}_{property}={arguments} {number}'.format(
-                type      = type,
-                property  = property,
-                arguments = arguments,
-                number    = number
-              ))
-
-        # finally, lets write dump to file
-        file = '{directory}/salt_metrics.{decorator}'.format(
-          directory = dirname( self.options.metrics.saved_path ),
-          decorator = self.decorator
-        )
-        with open( file, 'w' ) as file:
-          file.write("\n".join( dump ))
-
-    return locals()[self.decorator]
+      # finally, lets write dump to file
+      file = '{directory}/salt_metrics.{decorator}'.format(
+        directory = dirname( saved_path ),
+        decorator = self.decorator
+      )
+      with open( file, 'w' ) as file:
+        file.write("\n".join( dump )) 
--- tests/main.py
+++ tests/main.py
@@ -7,32 +7,74 @@
 import unittest
 import saltmetrics
 from saltmetrics import collector
+from saltmetrics import log
+from threading import Thread
+from time import sleep
+from functools import wraps
+from multiprocessing import Process
+
 
 # tests #########################################
 
 class MainTest( unittest.TestCase ):
   ''' Provides test cases for overall and expected functionality
   '''
-  
+  UPDATE_INTERVAL = 5
+
   def setUp( self ):
-    self.collector = collector({
-      '__role': 'master',
-      'transport': 'zeromq',
-      'sock_dir': '/tmp',
-      'metrics': {
-        'update_interval': 30,
-        'saved_path': '/tmp/salt_metrics.json',
-        'persistence': True,
-        'decorator': 'prometheus'
-      }
-    })
+    log.info( 'MainTest#setUp: start' )    
 
+    # NOTE: I don't understand what weird pythonic bullshit
+    # neccessitates this, but it is needed for using timeout
+    # decorator 
+    self.__name__ = 'MainTest'
 
+    # setup collector instance with enough options to be
+    # able to successfully instantiate
+    try:
+      self.collector
+
+    except:
+      log.info( 'MainTest#setup: exception' )
+      self.collector = collector({
+        '__role': 'master',
+        'transport': 'zeromq',
+        'sock_dir': '/tmp',
+        'pki_dir': '/etc/salt/pki',
+        'metrics': {
+          'update_interval': self.UPDATE_INTERVAL,
+          'saved_path': '/tmp/salt_metrics.json',
+          'persistence': True,
+          'decorator': 'prometheus'
+        }
+      })
+
+  def __timeout(seconds=15, error_message="Timeout"):
+    def decorator(func):
+      def wrapper(*args, **kwargs):
+        process = Process(None, func, None, args, kwargs)
+        process.start()
+        process.join(seconds)
+        if process.is_alive():
+          process.terminate()
+          raise Exception(error_message)
+
+      return wraps(func)(wrapper)
+    return decorator 
+
+  @__timeout
   def test_collector( self ):
-    self.assertEqual(type(self.collector), saltmetrics.collectors.DecoratedMetricsCollector)
-    
+    '''Ensure collector is correct instance type
+    '''
+    log.info( 'MainTest#test_collector: start' )
+    self.assertEqual(
+      type(self.collector), saltmetrics.collectors.DecoratedMetricsCollector
+    )
   
-  
+  def test_run( self ):
+    log.info( 'MainTest#test_run: start' )    
+    self.collector.run()
+
 # main ##########################################
 
 if __name__ == '__main__':
